f416b2f7c903ca09d996b7f05006cd2eedf0e312
Abhishek Narayan <narayan.abhishek.28@gmail.com>
2025-11-02 18:52:02 +0530

    Code update

--- InterviewPreparation/src/Java8/CollectionsExample.java
+++ InterviewPreparation/src/Java8/CollectionsExample.java
@@ -1,14 +1,120 @@
 package Java8;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import DataStructure.map.HashMap.ConcurrenthashMap;
 
-public class CollectionsExample {
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class CollectionsExample <T> {
+
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor){
+        Set<Object> set = ConcurrentHashMap.newKeySet();
+        return t-> set.add(keyExtractor.apply(t));
+    }
+
     //Java 8 added simplified immutable collection initialization methods, such as `List.of` and `Map.of`
     public static void main(String[] args) {
+
+        List<CollectorsExample.Person> people = Arrays.asList(
+                new CollectorsExample.Person("Abhishek", 32, "Bangalore"),
+                new CollectorsExample.Person("Anu", 32, "Bangalore"),
+                new CollectorsExample.Person("Dhriti", 2, "Bangalore"),
+                new CollectorsExample.Person("Narayan", 30, "Bangalore"),
+                new CollectorsExample.Person("Sah", 28, "Bangalore"),
+                new CollectorsExample.Person("Abhishek", 32, "Patna"),
+                new CollectorsExample.Person("Anu", 32, "Patna"),
+                new CollectorsExample.Person("Dhriti", 2, "Patna"),
+                new CollectorsExample.Person("Narayan", 30, "Patna"),
+                new CollectorsExample.Person("Sah", 28, "Patna")
+        );
+
+        //converting list<list<T>> into flat List<T>
+        List<List<?>> listOfList = List.of(List.of("Abhishek","Narayan"),List.of("Anu","Sah"));
+        System.out.println(listOfList.stream().flatMap(Collection::stream).toList());
+
+        //group a list of objects by a property
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+        Map<Integer, List<String>> groupedByLength = strings.stream()
+                .collect(Collectors.groupingBy(String::length));
+        System.out.println("Grouped by length: " + groupedByLength);
+
+        //sum of even number from an array of integers
+        int[] ar= {1,4,2,7,9,5};
+        int sum=Arrays.stream(ar)
+
+                .filter(even->even%2==0)
+                .reduce(0,Integer::sum);
+
+        System.out.println(sum);
+
+        // sum of odd numbers from a list of integers
+        List<Integer> ar1= Arrays.asList(1,4,2,7,9,5);
+        int sum1=ar1.stream()
+
+                .filter(odd->odd%2!=0)
+                .reduce(0,Integer::sum);
+
+        System.out.println(sum1);
+        // remove duplicates from list of int
+        List<Integer> listNum= Arrays.asList(1,4,5,9,1,2,7,9,5);
+        List <Integer> nonDup=listNum.stream()
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .toList();
+
+        System.out.println("non duplicate list: "+nonDup);
+
+        // remove duplicates from a stream based on a custom key
+        //we need to create a helper method
+
+        List<CollectorsExample.Person> newList = people.stream()
+                .filter(distinctByKey(CollectorsExample.Person::name))
+                .toList();
+        System.out.println("People list distinct by name: "+newList);
+
+        Map<String, CollectorsExample.Person> newList2 = people.stream()
+                .collect(Collectors.toMap(
+                        CollectorsExample.Person::name,
+                        Function.identity(),
+                                (existing, latest) -> existing
+                        ));
+
+        List<CollectorsExample.Person> listDistinctPerson = new ArrayList<>(newList2.values());
+        System.out.println("People list distinct by name using 2nd way: "+listDistinctPerson);
+
+        //count the number of characters in a character array using stream
+        char[] charArr = {'a', 'g', 's','a','r','s'};
+
+        Map<Character, Long> mapChar = IntStream.range(0, charArr.length)
+                .mapToObj(i->charArr[i])
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+        System.out.println("count of each char in "+ Arrays.toString(charArr) +" is: "+mapChar);
+
+        //finding the max value from a list of integer
+        int maxNum = listNum.stream().max(Comparator.naturalOrder()).get(); //option1
+        int n = listNum.stream().mapToInt(Integer::intValue).max().getAsInt(); //option2
+        int m = Collections.max(listNum); //option3
+
+        System.out.println("Max num in "+listNum+" is "+maxNum);
+
+        //getting top 2 values from a list of numbers after removing duplicate
+        //top N elements from a stream based on a comparator
+        List<Integer> top2 = listNum.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .limit(2)
+                .toList();
+
+        System.out.println("top 2 values in "+listNum+" is "+top2);
+
         List<String> namesList = List.of("Plice", "Bob", "Charlie");
         Map<String, Integer> ages = Map.of("Alice", 25, "Bob", 30);
 
